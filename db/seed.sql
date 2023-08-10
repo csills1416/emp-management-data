cee6b24166ad84d47fa5002ab8a3fd7c32e1524b
CSillus <71670415+csills1416@users.noreply.github.com>
2023-08-10 09:47:16 -0400

    updated seeds.sql to contain employee information.

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,28 @@
+INSERT INTO departments (name) VALUES 
+('Sales'),
+('Engineering'),
+('Marketing')
 
+INSERT INTO roles (title, salary, department_id) VALUES
+
+('Sales Lead', 100000, 1),
+
+('Salesperson', 80000, 1),
+
+('Lead Engineer', 150000, 2),
+
+('Software Engineer', 120000, 2),
+
+('Account Manager', 160000, 3),
+
+INSERT INTO employees (first_name, last_name, role_id, department_id) VALUES
+
+('John', 'Doe', 1, 1), --Sales Lead
+
+('Mike', 'Chan', 2, 1), --Salesperson
+
+('Ashley', 'Rodriguez', 3, 2), --Lead Engineer
+
+('Kevin', 'Tupik', 4, 2), --Software Engineer
+
+('Kimberly', 'Clark', 5, 3), --Account Manager
