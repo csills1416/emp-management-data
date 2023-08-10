d46b8706030ebca6c7b2ca9cfbed888b19419bde
CSillus <71670415+csills1416@users.noreply.github.com>
2023-08-10 14:56:27 -0400

    added function to display data and exec. SQL scripts from schema and seed.

--- server.js
+++ server.js
@@ -2,50 +2,72 @@ const express = require('express');
 const mysql = require('mysql');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-// Create a MySQL connection
 const db = mysql.createConnection({
   host: 'company',
   user: 'root',
   password: '',
-  database: 'employees' // Change this to your database name
 });
 
-// Connect to the MySQL database
 db.connect(err => {
   if (err) {
     console.error('Error connecting to database:', err);
   } else {
     console.log('Connected to database');
+    createDatabaseAndTables();
   }
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Define route to display employees in a table
-app.get('/employees', (req, res) => {
-  const query = 'SELECT id, first_name, last_name FROM employees';
-
-  db.query(query, (err, results) => {
+function createDatabaseAndTables() {
+  const schemaSql = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8');
+  db.query(schemaSql, (err, results) => {
     if (err) {
-      console.error('Error fetching employees:', err);
-      res.status(500).send('Error fetching employees');
+      console.error('Error creating tables:', err);
     } else {
-      res.render('employees', { employees: results });
+      console.log('Tables created');
+      populateDatabase();
     }
   });
-});
+}
 
-// Define a default route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Employee Management System');
-});
+function populateDatabase() {
+  const seedSql = fs.readFileSync(path.join(__dirname, 'seed.sql'), 'utf8');
+  db.query(seedSql, (err, results) => {
+    if (err) {
+      console.error('Error populating data:', err);
+    } else {
+      console.log('Data populated');
+      startServer();
+    }
+  });
+}
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+function startServer() {
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
+
+  app.get('/employees', (req, res) => {
+    const query = 'SELECT id, first_name, last_name FROM employees';
+
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Error fetching employees:', err);
+        res.status(500).send('Error fetching employees');
+      } else {
+        res.render('employees', { employees: results });
+      }
+    });
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to the Employee Management System');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
