731a0b09b0beb2ac353c32eb006ea0cb5028ea97
CSillus <71670415+csills1416@users.noreply.github.com>
2023-08-13 07:05:39 -0400

    changed throw err, added function.

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const connection = mysql.createConnection({
 
 // Establish the connection
 connection.connect(err => {
-    if (err) throw err;
+    if (err) {
+        throw err; // Added brackets around the if block
+    }
     console.log('Connected to the database.');
     startApp();
 });
@@ -26,25 +28,101 @@ function startApp() {
               message: 'Choose an action:',
               choices: [
                   'View all departments',
-                  // Other choices...
+                  'View all Roles',
+                  'View all Employees',
+                  'Add a department',
+                  'Add a role',
+                  'Add an employee',
+                  'Update an employee role'
               ],
           },
       ])
       .then(answer => {
-          if (answer.action === 'View all departments') {
-              viewAllDepartments(); // Call the function
+          switch (answer.action) {
+              case 'View all departments':
+                  viewAllDepartments();
+                  break;
+              case 'View all Roles':
+                  viewAllRoles();
+                  break;
+              case 'View all Employees':
+                  viewAllEmployees();
+                  break;
+              case 'Add a department':
+                  addDepartment();
+                  break;
+              case 'Add a role':
+                  addRole();
+                  break;
+              case 'Add an employee':
+                  addEmployee();
+                  break;
+              case 'Update an employee role':
+                  updateEmployeeRole();
+                  break;
+              default:
+                  connection.end(); // Close the connection on exit
+                  console.log('Goodbye!');
           }
-          // Handle other actions...
       })
       .catch(err => console.error(err));
 }
 
 function viewAllDepartments() {
   connection.query('SELECT * FROM departments', (err, results) => {
-      if (err) throw err;
+      if (err) {
+          throw err; // Added brackets around the if block
+      }
       console.table(results);
-      startApp(); // Continue the loop
+      startApp();
   });
 }
+
+function viewAllRoles() {
+    connection.query('SELECT * FROM roles', (err, results) => {
+        if (err) {
+            throw err; // Added brackets around the if block
+        }
+        console.table(results);
+        startApp();
+    });
+}
+
+function viewAllEmployees() {
+    connection.query('SELECT * FROM employees', (err, results) => {
+        if (err) {
+            throw err; // Added brackets around the if block
+        }
+        console.table(results);
+        startApp();
+    });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter the name of the department:',
+      },
+    ])
+    .then(answer => {
+      connection.query(
+        'INSERT INTO departments (name) VALUES (?)',
+        [answer.departmentName],
+        err => {
+          if (err) {
+            throw err; // Added brackets around the if block
+          }
+          console.log('Department added successfully.');
+          startApp();
+        }
+      );
+    })
+    .catch(err => console.error(err));
+}
+
+// Implement similar functions for adding roles, employees, and updating employee roles.
 
 // Call the startApp function to initiate the application
